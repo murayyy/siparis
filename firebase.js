68f4773aec2f9dcd477beb48990e7f96c287f863
murayyy <m.demirtas34@hotmail.com>
2025-09-27 12:24:16 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,42 +1,45 @@
+// Firebase SDK importları
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  signOut, 
+  onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-import {
-  getFirestore,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  query,
-  where,
-  addDoc,
-  updateDoc,
-  serverTimestamp,
-  orderBy
+import { 
+  getFirestore, 
+  collection, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  getDocs, 
+  query, 
+  where, 
+  addDoc, 
+  updateDoc, 
+  serverTimestamp, 
+  orderBy 
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// 🔑 Senin config
+// Senin gerçek config’in
 const firebaseConfig = {
-  apiKey: "AIzaSyDcLQ84UggX1YA9x8AKw-XybJjcF6U_KA4",
+  apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com",
+  storageBucket: "depo1-4668f.appspot.com",   // 🔧 dikkat: ".app" değil ".app**spot.com**"
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
 };
 
+// Başlat
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
+// Dışa aktar
 export {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
