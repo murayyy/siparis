4a13815e05a5ccebc6822a2d0a56906468f1a2a1
murayyy <m.demirtas34@hotmail.com>
2025-09-28 13:54:04 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase.js (CDN, modüler)
+// Firebase SDK importları (modüler)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import {
   getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged
@@ -8,20 +8,23 @@ import {
   query, where, orderBy, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+// 🔧 Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com", // ← BU SATIR BU ŞEKİLDE OLMALI
+  storageBucket: "depo1-4668f.appspot.com", // 🔥 appspot.com
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
 };
 
+// Başlat
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db   = getFirestore(app);
+const db = getFirestore(app);
 
+// Dışa aktar
 export {
   app, auth, db,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged,
