551a6f8eb6ebc5665898cdc31921cc671fc2037f
murayyy <m.demirtas34@hotmail.com>
2025-09-28 13:14:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -28,7 +28,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com",   // ✅ doğru olan
+  storageBucket: "depo1-4668f.firebasestorage.app",   // ✅ doğru olan
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
