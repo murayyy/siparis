d90892c364b4b658f4198442a2efee52e81b5a50
murayyy <m.demirtas34@hotmail.com>
2025-09-28 12:11:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,59 +1,35 @@
-// Firebase SDK importları
+// Firebase SDK importları (modüler)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { 
-  getAuth, 
-  signInWithEmailAndPassword, 
-  createUserWithEmailAndPassword, 
-  signOut, 
-  onAuthStateChanged 
+import {
+  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import { 
-  getFirestore, 
-  collection, 
-  doc, 
-  setDoc, 
-  getDoc, 
-  getDocs, 
-  query, 
-  where, 
-  addDoc, 
-  updateDoc, 
-  serverTimestamp, 
-  orderBy 
+import {
+  getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, addDoc,
+  query, where, orderBy, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Senin gerçek config’in
+// 🔧 Senin config’in (gerekirse değiştir)
 const firebaseConfig = {
   apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com",   // 🔧 dikkat: ".app" değil ".app**spot.com**"
+  storageBucket: "depo1-4668f.appspot.com",
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
 };
 
 // Başlat
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Dışa aktar
+// Dışa aktar (app.js burada kullansın)
 export {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  query,
-  where,
-  addDoc,
-  updateDoc,
-  serverTimestamp,
-  orderBy
+  app, auth, db,
+  // Auth
+  signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged,
+  // Firestore
+  collection, doc, setDoc, getDoc, getDocs, updateDoc, addDoc,
+  query, where, orderBy, serverTimestamp
 };
