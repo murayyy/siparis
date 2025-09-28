cc245861bcb33c8b7aacb3f48c3f730f148d8e94
murayyy <m.demirtas34@hotmail.com>
2025-09-28 14:29:08 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,30 @@
+// ================= FIREBASE INIT =================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {
-  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged
+import { 
+  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import {
+import { 
   getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, addDoc,
   query, where, orderBy, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+// 🔧 Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com",
+  storageBucket: "depo1-4668f.appspot.com",   // ✅ düzelttim
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
 };
 
+// Başlat
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Dışa aktar
 export {
   app, auth, db,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged,
