1cb829141285f3feb69449ec102a080bcba9ea1a
murayyy <m.demirtas34@hotmail.com>
2025-09-29 16:51:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById("menuToggle")?.addEventListener("click", () => {
 
 // ================= FIREBASE IMPORT =================
 import { 
-  auth, db,
+ app, auth, db,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged,
   collection, doc, setDoc, getDoc, getDocs, updateDoc, addDoc,
   query, where, serverTimestamp
