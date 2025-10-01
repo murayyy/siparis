e21bbb9d808062e54f5eef25a0acab5b088426a2
murayyy <m.demirtas34@hotmail.com>
2025-10-01 17:45:32 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,23 +8,20 @@ import {
   query, where, orderBy, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// 🔧 Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDcLQB4UggXlYA9x8AKw-XybJjcF6U_KA4",
   authDomain: "depo1-4668f.firebaseapp.com",
   projectId: "depo1-4668f",
-  storageBucket: "depo1-4668f.appspot.com",   // ✅ düzelttim
+  storageBucket: "depo1-4668f.appspot.com",
   messagingSenderId: "1044254626353",
   appId: "1:1044254626353:web:148c57df2456cc3d9e3b10",
   measurementId: "G-DFGMVLK9XH"
 };
 
-// Başlat
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Dışa aktar
 export {
   app, auth, db,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged,
