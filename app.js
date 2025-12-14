0c631bd19a6b105c535d3f918b4c9aea5d6a845f
murayyy <m.demirtas34@hotmail.com>
2025-12-14 16:36:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1522,9 +1522,6 @@ function closePickingDetailModal() {
       completeBtn.classList.toggle("opacity-50", !fromPicking);
       completeBtn.classList.toggle("cursor-not-allowed", !fromPicking);
     }
- 
-    console.error("openPickingDetailModal hata:", err);
-    showGlobalAlert("Sipariş detayları yüklenemedi: " + err.message);
 }
 
 // --------------------------------------------------------
